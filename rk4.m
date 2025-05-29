613fcbfc34ad747bdeaf79e516e42443f8694692
AyimanyMooncat <mooncatayimany@proton.me>
2025-05-29 10:38:22 -0600

    Correct RK4

--- rk4.m
+++ rk4.m
@@ -7,7 +7,8 @@ nst = length(xv);
 
 yv = zeros(1, nst);
 
-dydx = @(x, y) -2 * x^3 + 12 * x^2 - 20 * x + 8.5;
+dydx = @(x, y) -2 * y + x^3 * exp(-2 * x);
+y = @(x) (exp(-2 * x))/(4) * (x^4 + 4);
 
 xv(1) = 0;
 yv(1) = 1;
@@ -16,7 +17,7 @@ yv(1) = 1;
 for i = 1 : nst - 1
     k1 = dydx(xv(i), yv(i));
     k2 = dydx(xv(i) + 0.5 * dx, yv(i) + 0.5 * k1 * dx);
-    k3 = dydx(xv(i) + 0.5 * dx, yv(i) + 0.5 * k2 * h);
+    k3 = dydx(xv(i) + 0.5 * dx, yv(i) + 0.5 * k2 * dx);
     k4 = dydx(xv(i) + dx, yv(i) + k3 * dx);
     yv(i + 1) = yv(i) + (1/6) * (k1 + 2 * k2 + 2 * k3 + k4) * dx;
 end
